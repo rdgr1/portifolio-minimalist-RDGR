5423b446f17711b270b094640da756bb73b7127f
Rodger <rvidaldias@gmail.com>
2025-01-21 14:45:04 -0300

    add: init styles of index header

---
+++ src/app/components/header-index/header-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-index.component.html',
+  styleUrl: './header-index.component.scss'
+})
+export class HeaderIndexComponent {
+
+}
