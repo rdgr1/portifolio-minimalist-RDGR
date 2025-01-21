6e1431090f2b2cb10585b5098108ed991fd57f59
Rodger <rvidaldias@gmail.com>
2025-01-21 14:43:45 -0300

    add: create initial struc of header and init style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HeaderIndexComponent } from './components/header-index/header-index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    "path":"index",
+    "component": HeaderIndexComponent
+}];
