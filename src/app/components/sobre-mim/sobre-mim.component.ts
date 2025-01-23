090a52c4f229275d55b2061ced9f8e1a2390e9a6
Rodger <rvidaldias@gmail.com>
2025-01-23 14:44:39 -0300

    feat: creat and finish inital layout of about me

--- src/app/components/sobre-mim/sobre-mim.component.ts
+++ src/app/components/sobre-mim/sobre-mim.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LanguageStatusBarComponent } from '../language-status-bar/language-status-bar.component';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-sobre-mim',
   standalone: true,
-  imports: [],
+  imports: [LanguageStatusBarComponent, NavBarComponent],
   templateUrl: './sobre-mim.component.html',
   styleUrl: './sobre-mim.component.scss'
 })
