f47f5d3c12d80b9eb14f93891ba3964452b6fc19
Rodger <rvidaldias@gmail.com>
2025-01-24 09:49:09 -0300

    feat: test for live preview coding front-end

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { SobreMimComponent } from "../../components/sobre-mim/sobre-mim.component";
+import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+import { SetaComponentComponent } from "../../components/seta-component/seta-component.component";
+import { CardProjetoComponent } from "../../components/card-projeto/card-projeto.component";
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [SobreMimComponent],
+  imports: [SobreMimComponent, NavBarComponent, SetaComponentComponent, CardProjetoComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
