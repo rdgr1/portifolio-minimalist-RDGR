2669c7fbb9e5477fd69b2536f484efa0c00590ec
Rodger <rvidaldias@gmail.com>
2025-01-24 12:19:27 -0300

    remove unused import

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { InputFormularioComponent } from "../../components/input-formulario/input-formulario.component";
 import { FormularioLayoutComponent } from "../../components/formulario-layout/formulario-layout.component";
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [InputFormularioComponent, FormularioLayoutComponent,ReactiveFormsModule],
+  imports: [FormularioLayoutComponent, ReactiveFormsModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
