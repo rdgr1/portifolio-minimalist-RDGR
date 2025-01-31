316d8d5910adf96a294c0c805c332af591c7b29c
Rodger <rvidaldias@gmail.com>
2025-01-31 14:04:10 -0300

    feat: add new page and add animation for the best projects

--- src/app/components/carrousel-layout/carrousel-layout.component.ts
+++ src/app/components/carrousel-layout/carrousel-layout.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, AfterViewInit, ElementRef, ViewChild, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-carrousel-layout',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carrousel-layout.component.html',
   styleUrl: './carrousel-layout.component.scss'
 })
-export class CarrouselLayoutComponent {
+export class CarrouselLayoutComponent implements AfterViewInit {
+  @ViewChild('slider') slider!: ElementRef;
 
+  ngAfterViewInit() {
+    const sliderElement = this.slider.nativeElement;
+
+    sliderElement.addEventListener('mouseenter', () => {
+      sliderElement.style.animationPlayState = 'paused'; // Pausa a animação corretamente
+    });
+
+    sliderElement.addEventListener('mouseleave', () => {
+      sliderElement.style.animationPlayState = 'running'; // Retoma a animação sem resetar
+    });
+  }
 }
