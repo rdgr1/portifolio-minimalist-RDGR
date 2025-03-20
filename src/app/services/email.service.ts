a9c90c4b8f5d6df6cf8256e9e935492d06a1cf18
rdgr1 <rodgerdias6@gmail.com>
2025-03-20 11:27:09 -0300

    feat: add new back end instance

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -4,17 +4,25 @@ import { Observable } from 'rxjs';
 import { ContatoResponse } from '../types/contato-response.type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmailService {
-  private apiUrl: string = "https://contato-spring-back-end-portifolio.onrender.com/api/emails/send";
+  private apiUrl: string =
+    'https://contatospring-back-end-portifol-production.up.railway.app/api/emails/send';
 
   constructor(private httpClient: HttpClient) {}
 
-  submitForm(name: string, email: string, message: string): Observable<ContatoResponse> {
+  submitForm(
+    name: string,
+    email: string,
+    message: string
+  ): Observable<ContatoResponse> {
     const formData = { name, email, message };
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    return this.httpClient.post<ContatoResponse>(this.apiUrl, formData, { headers, responseType: 'json' });
+    return this.httpClient.post<ContatoResponse>(this.apiUrl, formData, {
+      headers,
+      responseType: 'json',
+    });
   }
 }
